0809f6c4fdfbe5a571e9db7f499f35235f5ba311
John Kersey <middlejohn@yahoo.com>
2022-09-18 07:32:27 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+const characters = {
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  number: "0123456789",
+  specialCha: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
