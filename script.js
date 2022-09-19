bdcbd3b3bb49d07bda32477d4ce681e2a74d3967
John Kersey <middlejohn@yahoo.com>
2022-09-19 05:13:05 -0400

    Notes blocking out project and small progress

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 const characters = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -8,14 +5,36 @@ const characters = {
   specialCha: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
 }
 
+// First, input prompt for character length. Then bool prompt for character types. 
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");  // connects to ID on the button so we can listen for a button click. 
+var lowBool = true;
+var upBool = true;
+var numBool = true;
+var speBool = true;
+var passwordLength = 8
+
+// window.confirm("sometext");     
+// if (confirm("Press a button!")) {
+// txt = "You pressed OK!";
+// } else {
+//  txt = "You pressed Cancel!";
+// }
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword({  // generate password here. 
 
-  passwordText.value = password;
+  });
+  var passwordText = document.querySelector("#password"); // targets password id 
+  passwordText.value = password; //rewrites placeholder text to generated password 
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);   // This listens for button click, the runs writePassword function. 
+
+
+// 
