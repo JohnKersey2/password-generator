aa15ec06c501533420292b13712855bdfa6320a0
John Kersey <middlejohn@yahoo.com>
2022-09-19 08:28:18 -0400

    Cleaned up notes and refactored code.

--- script.js
+++ script.js
@@ -2,37 +2,40 @@ const characters = {
   lowercase: "abcdefghijklmnopqrstuvwxyz",
   uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   number: "0123456789",
-  specialCha: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+  specCha: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
 }
 
-// First, input prompt for character length. Then bool prompt for character types. 
-
-// Assignment Code
 var generateBtn = document.querySelector("#generate");  // connects to ID on the button so we can listen for a button click. 
-var lowBool = true 
-var upBool = true 
-var numBool = true 
-var specBool = true 
-var passwordLength = 8
+var lowBool
+var upBool
+var numBool
+var specBool 
+var passwordLength 
 
 
 function generatePassword() { 
   var passwordPrompt = (parseInt(prompt ("How many characters would you like your password to be?", "8 to 128"))) // creates window prompt for character limit and changes user input to an int
-  if (passwordPrompt >= 8 && passwordPrompt <= 128 ) {
+  if (passwordPrompt >= 8 && passwordPrompt <= 128 ) { // If user submits valid number, continues on to generate password 
     passwordLength = passwordPrompt
+
+    // Confirms which characters the user wants
     lowBool = confirm ("Do you want lowercase letters in your password?")
     upBool = confirm ("Do you want uppercase letters in your password?")
     numBool = confirm ("Do you want numbers in your password?")
     specBool = confirm ("Do you want special characters in your password?")
+
+
+
+    return ;
   }
-  else {
-    alert ("Please check that you picked a number between 8 and 128 and try again.")
+  else { // Returns a try again message instead of a password
+    return "Please check that you picked a number between 8 and 128 and try again.";
   }
-  return ;
+  
 } 
 
 
-// Write password to the #password input
+// Function that calls generate password function, then writes the returned value into the html file
 function writePassword() {
   var password = generatePassword();
   
@@ -44,10 +47,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);   // This listens for button click, the runs writePassword function. 
 
-
-// window.confirm("sometext");     
-// if (confirm("Press a button!")) {
-// txt = "You pressed OK!";
-// } else {
-//  txt = "You pressed Cancel!";
-// }
